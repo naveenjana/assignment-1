abbeef267fe91a3bc6a6dc0554cb4b4def04c64c
Naveen kumar <95871049+naveenjana@users.noreply.github.com>
2022-01-20 17:35:13 -0800

    Add files via upload

---
+++ assignment3/assignment 3/Celsius.java
@@ -0,0 +1,10 @@
+class Celsius
+{
+public static void main(String[]args)
+{
+int Fahrenheit=212,Celsius;
+          
+          Celsius  = ((Fahrenheit-32)*5)/9;  
+          System.out.println("Temperature in celsius is: "+Celsius); 
+}
+}
