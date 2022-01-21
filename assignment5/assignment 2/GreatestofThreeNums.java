9d5d517b329c11a5d0909bfbdcd9d414c4b8ae5d
Naveen kumar <95871049+naveenjana@users.noreply.github.com>
2022-01-20 17:38:05 -0800

    Add files via upload

---
+++ assignment5/assignment 2/GreatestofThreeNums.java
@@ -0,0 +1,18 @@
+class GreatestofThreeNums 
+{
+public static void main(String[] args) 
+{
+int a=10;
+int b=20;
+int c=30;
+if(a>b)
+if(a>c)
+System.out.println("The greatest: " +a);
+if(b>a)
+if(b>c)
+System.out.println("The greatest: " +b);
+if(c>a)
+if(c>b)
+System.out.println("The greatest: " +c);
+}
+}
